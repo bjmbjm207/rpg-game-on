297c50916ad390a5c3c6509cabeaaa4ba2c35ed3
Minh Thi <bjmbjm207@gmail.com>
2021-11-27 00:02:38 +0700

    Specs of Player and Enemy + EnemyEffectDeath
    12h02 27-Nov-2021

--- Godot-RPG-Story-Of-TP/World/Grass.gd
+++ Godot-RPG-Story-Of-TP/World/Grass.gd
@@ -1,10 +1,8 @@
 extends Node2D
-
+var GrassEffect = preload("res://Effects/GrassEffect.tscn")
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	get_parent().add_child(grassEffect) 
 	grassEffect.global_position = global_position
 	queue_free()
 
